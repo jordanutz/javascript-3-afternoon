50e6312d380b3b10cf72bade3ff6770e51e9a85a
jordanutz <jordanutz@outlook.com>
2019-01-09 15:10:54 -0700

    Completed Classes

--- classes.js
+++ classes.js
@@ -1,4 +1,4 @@
-/* 
+/*
   Once you complete a problem, refresh ./classes.html in your browser and check to see if the problem's test(s) are passing.
   Passed tests will be indicated by a green circle.
   Failed tests will be indicated by a red X.
@@ -29,8 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
-
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+  makeWidget (first_name, last_name) {
+    return this.first_name + ' ' + this.last_name + ' ' + 'Widget'
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -49,7 +58,24 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager {
+  constructor(first_name, last_name, email, age, reports) {
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports = []
+  }
+  makeWidget (first_name, last_name) {
+    return this.first_name + ' ' + this.last_name + ' ' + 'Widget'
+  }
+  hire (employee) {
+    this.reports.push(employee)
+  }
+  fire (index) {
+    this.reports.splice(index, 1)
+  }
+}
 
 
 
@@ -75,9 +101,40 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
-
-
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age, reports, title, bonus) {
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports = []
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+  makeWidget (first_name, last_name) {
+    return this.first_name + ' ' + this.last_name + ' ' + 'Widget'
+  }
+  hire (employee) {
+    this.reports.push(employee)
+    if (this.reports.length >= 101) {
+      this.title = 'Bestest Manager'
+    } else if (this.reports.length <= 100 && this.reports.length >= 51) {
+      this.title = 'Manager Plus'
+    } else if (this.reports.length <= 50 && this.reports.length >= 11) {
+      this.title = 'Manager'
+    } else if (this.reports.length <= 10 && this.reports.length >= 4) {
+      this.title = 'Mostly Manager'
+    } else if (this.reports.length <= 3 && this.reports.length >= 1) {
+      this.title = 'Barely Manager'
+    } else if (this.reports.length = 0) {
+      this.title = 'Not a manager'
+    }
+  }
+  fire (index) {
+    this.reports.splice(index, 1)
+    this.bonus += 100
+  }
+}
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
@@ -102,6 +159,23 @@
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
-
-
+class Machine {
+  constructor() {
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+  makeWidgets (num) {
+    this.widgets_made_count += num
+    this.wear_and_tear_count = this.widgets_made_count / 50
+  }
+  fixMachine () {
+    this.needs_reboot = true
+  }
+  reboot () {
+    return () => {
+      this.wear_and_tear_count -= 10
+      this.needs_reboot = false
+    }
+  }
+}
